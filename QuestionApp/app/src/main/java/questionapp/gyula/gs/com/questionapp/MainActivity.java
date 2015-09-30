83c255be176bb23971c05ad0b243d78049957cf4
soosgyul <soosgyul@gmail.com>
2015-09-30 11:43:23 +0100

    advancing with thequestion app

--- QuestionApp/app/src/main/java/questionapp/gyula/gs/com/questionapp/MainActivity.java
+++ QuestionApp/app/src/main/java/questionapp/gyula/gs/com/questionapp/MainActivity.java
@@ -4,34 +4,63 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //variables go here
+    private Button btnFalse;
+    private Button btnTrue;
+    private TextView lblQuestion;
+    private ImageView imgPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //connect variables to interface items
+        btnFalse = (Button) findViewById(R.id.btnFalse);
+        btnTrue = (Button) findViewById(R.id.btnTrue);
+        lblQuestion = (TextView) findViewById(R.id.lblQuestion);
+        imgPicture = (ImageView) findViewById(R.id.imgPicture);
+
+        //set questionare question
+        lblQuestion.setText("Am I Gyula?");
+
+        //set image picture
+        imgPicture.setImageResource(R.drawable.gyula);
+
+        //onclick listeners
+        btnFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "nope!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "correct!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+//this part
+    private void generateQuestions(){
+
+        questions = ArrayList<>();
+
+        questions.add(new QuestionObject("Is this an iOS phone?", "yes", "no", false, R.drawable.android));
+
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+}
 }
